4a3718341e172a54d1cebf3acf2aea5cb77d9ee5
Madro <daanii2013@gmail.com>
2021-08-01 20:15:28 -0500

    adding portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,3 +92,18 @@ modalCloses.forEach((modalClose)=>{
         })
     })
 });
+
+// ======PORTAFOLIO======
+// swiper
+let swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+});
